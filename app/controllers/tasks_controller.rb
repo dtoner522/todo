5a7780db4f90d06c7b2780448d482ae4fabe06c1
daire toner <dtoner522@gmail.com>
2021-03-14 12:53:41 +0100

    CRUD functionality all working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,14 +12,35 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(tasks_params)
+    if @task.save
+      redirect_to tasks_path(@task)
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    #add alert here!
+    redirect_to tasks_path
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-  endS
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path(@task)
+    # add alert here!
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details)
+  end
 end
